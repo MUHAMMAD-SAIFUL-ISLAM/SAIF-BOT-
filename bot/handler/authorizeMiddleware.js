d0fc75b30e89297083814727e9beed1be54ce565
Muhammad Saiful islam <muhammadsaifuli169@gmail.com>
2025-10-13 17:25:42 +0600

    Update authorizeMiddleware.js

--- bot/handler/authorizeMiddleware.js
+++ bot/handler/authorizeMiddleware.js
@@ -2,11 +2,9 @@ module.exports = async function ({ event, message }) {
     const threadID = event.threadID;
     const threadData = global.db.allThreadData.find(t => t.threadID == threadID);
 
-    // যদি group authorize না থাকে
     if (!threadData || !threadData.data.isAuthorized) {
         await message.reply("❌ Group not authorized. Please contact admin.");
-        return false; // command execution stop
+        return false; // command stop
     }
-
-    return true; // command চলতে পারবে
+    return true; // proceed normally
 };
